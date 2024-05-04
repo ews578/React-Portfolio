a1d889d12bc5a3ac91e4dd5761101c8f2f38092d
ews578 <ewsnider28@yahoo.com>
2024-05-04 10:47:24 -0500

    corrected navtabs and resume, added another project to portfolio,

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -4,6 +4,8 @@ export default function Portfolio() {
     <div>
       <h1> Project Page</h1>
       <Project title= "project 1" description= "description 1" img= "https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920_640.jpg" link= "www.google.com"></Project>
+      <Project title= "project 2" description= "description 2" img= "https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920_640.jpg" link= "www.google.com"></Project>
+
     </div>
   );
 }

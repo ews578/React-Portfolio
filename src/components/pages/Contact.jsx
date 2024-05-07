9f81e1b35ef28ca03f4f8dc48654d67bb18d7058
ews578 <ewsnider28@yahoo.com>
2024-05-07 14:22:05 -0500

    styled contact form. still needs some work

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,20 +1,53 @@
-export default function Contact() {
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+
+function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
+    <Form>
+      <Row className="mb-3">
+        <Col sm={12} md={6}>
+          <Form.Group controlId="formGridFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control placeholder="First name" />
+          </Form.Group>
+        </Col>
+
+        <Col sm={12} md={6}>
+          <Form.Group controlId="formGridLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control placeholder="Last Name" />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Col sm={12} md={6}>
+          <Form.Group className="mb-3" controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control placeholder="email" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col sm={12} md={6}>
+          <FloatingLabel controlId="floatingTextarea2" label="Comments">
+            <Form.Control
+              as="textarea"
+              placeholder="Leave a comment here"
+              style={{ height: "100px" }}
+            />
+          </FloatingLabel>
+        </Col>
+      </Row>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
+
+export default Contact;

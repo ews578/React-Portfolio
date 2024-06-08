274887457864a11dc99b2427925c890c4bcf6be6
ews578 <ewsnider28@yahoo.com>
2024-06-08 10:37:22 -0500

    added a profile picture and more styling

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,15 +1,28 @@
-export default function AboutMe() {
+import React from "react";
+
+function Aboutme() {
   return (
-    <div>
-      <h1>About Me Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <section id="about-me" className="my-5 intro">
+      <div class="flex-row">
+        <h1 id="about" className="section-title primary-border">
+          About Me
+        </h1>
+      </div>
+      <div className="flex-row">
+        <div class="intro-info">
+          <div class="intro-img">
+            <img src="assets/profilePicture.png" style={{ width: "100%" }} alt="profile" />
+          </div>
+          <p>
+          info here
+          </p>
+          <p>more info here</p>
+          <p>even more info here</p>
+          <p>holy moly even more info here</p>
+        </div>
+      </div>
+    </section>
   );
 }
+
+export default Aboutme;

c78eda5ea35513070717141c281fe87566785411
ews578 <ewsnider28@yahoo.com>
2024-06-09 08:34:54 -0500

    added react icon library and created a footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import PortfolioContainer from "./components/PortfolioContainer";
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => (
     <>
       <Header />
       <PortfolioContainer />
+      <Footer />
     </>
   );
   
---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { VscGithubAlt } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer>
+      <h2>Designed by Eric Snider©</h2>
+      <p>
+        <ul>
+          <a href="https://github.com/ews578">
+            <li className="logo">
+              <VscGithubAlt />
+            </li>
+          </a>
+          <a href="https://www.linkedin.com/in/eric-snider-b28656307">
+            <li className="logo">
+              <AiOutlineLinkedin />
+            </li>
+          </a>
+          <a href="https://www.instagram.com/">
+            <li className="logo">
+              <FaInstagram />
+            </li>
+          </a>
+        </ul>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
